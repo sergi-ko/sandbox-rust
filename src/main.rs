2560b1995a05132957f6535b3f8e8da036e84654
skopchalyuk <skopchalyuk@gmail.com>
2021-04-18 17:57:46 +0200

    using deadlock mod

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+pub mod deadlock;
 pub mod test;
 
 use crate::test::test1;
 use crate::test::test2;
+
+use crate::deadlock::do_something;
 
 fn main() {
     let s = test1();
     println!("{}", s);
 
     println!("{}", test2());
+
+    println!("{}", do_something());
 }
