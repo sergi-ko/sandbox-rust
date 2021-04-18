a4870b3a4fbb9607a4ef1125105e2d1b98373826
skopchalyuk <skopchalyuk@gmail.com>
2021-04-18 18:25:51 +0200

    task1 and task2 are using two ArcMutexes

--- src/deadlock.rs
+++ src/deadlock.rs
@@ -7,7 +7,7 @@ pub fn do_something() -> &'static str {
     let mut threads = vec![];
 
     let res1 = Arc::new(Mutex::new(0));
-    let res2 = Arc::new(Mutex::new(1));
+    let res2 = Arc::new(Mutex::new(0));
 
     let res1c = Arc::clone(&res1);
     let res2c = Arc::clone(&res2);
@@ -21,18 +21,29 @@ pub fn do_something() -> &'static str {
         handle.join().unwrap();
     }
 
+    println!("{}", *res1.lock().unwrap());
+    println!("{}", *res2.lock().unwrap());
+
     "doing something"
 }
 
 pub fn run_task1(res1: Arc<Mutex<u8>>, res2: Arc<Mutex<u8>>) -> JoinHandle<()> {
-    thread::spawn(|| {
+    thread::spawn(move || {
+        let mut task1_res1 = res1.lock().unwrap();
+        *task1_res1 += 1;
+        let mut task1_res2 = res2.lock().unwrap();
+        *task1_res2 += 1;
         thread::sleep(time::Duration::from_millis(2_000));
         println!("running task1");
     })
 }
 
 pub fn run_task2(res1: Arc<Mutex<u8>>, res2: Arc<Mutex<u8>>) -> JoinHandle<()> {
-    thread::spawn(|| {
+    thread::spawn(move || {
+        let mut task1_res1 = res1.lock().unwrap();
+        *task1_res1 += 10;
+        let mut task1_res2 = res2.lock().unwrap();
+        *task1_res2 += 10;
         thread::sleep(time::Duration::from_millis(3_000));
         println!("running task2");
     })
