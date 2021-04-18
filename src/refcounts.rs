04ecc56cd54674df80494a2530751e5b0b2f6eb6
skopchalyuk <skopchalyuk@gmail.com>
2021-04-19 00:37:03 +0200

    added refcounts

--- src/refcounts.rs
+++ src/refcounts.rs
@@ -1,1 +1,14 @@
-pub fn do_refcount() {}
+use std::rc::Rc;
+
+enum MyTree {
+    Leaf(u8, Rc<MyTree>, Rc<MyTree>),
+    Nil,
+}
+
+pub fn do_refcount() {
+    let tree = MyTree::Leaf(
+        5,
+        Rc::new(MyTree::Nil),
+        Rc::new(MyTree::Leaf(10, Rc::new(MyTree::Nil), Rc::new(MyTree::Nil))),
+    );
+}
