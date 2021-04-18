2d19a1fa83923c7be795bcf4b207ffcb020de302
skopchalyuk <skopchalyuk@gmail.com>
2021-04-18 18:08:41 +0200

    added two tasks to deadlock mod

--- src/deadlock.rs
+++ src/deadlock.rs
@@ -1,3 +1,26 @@
+use std::thread;
+use std::thread::JoinHandle;
+
 pub fn do_something() -> &'static str {
+    let mut threads = vec![];
+    threads.push(run_task1());
+    threads.push(run_task2());
+
+    for handle in threads {
+        handle.join();
+    }
+
     "doing something"
+}
+
+pub fn run_task1() -> JoinHandle<()> {
+    thread::spawn(|| {
+        println!("running task1");
+    })
+}
+
+pub fn run_task2() -> JoinHandle<()> {
+    thread::spawn(|| {
+        println!("running task2");
+    })
 }
