92700cf08b04570695fb771fb839be8dd2a33269
skopchalyuk <skopchalyuk@gmail.com>
2021-04-19 08:29:36 +0200

    made performance depend on user input

--- src/iter.rs
+++ src/iter.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::iter::FromIterator;
 use std::time::Instant;
 
@@ -31,80 +32,93 @@ pub struct VecStruct {
 //
 // For more information about this error, try `rustc --explain E0507`.
 
-pub fn struct_iter_iter(vstruct: &VecStruct) -> u128 {
-    let mut k = 0;
+pub fn struct_iter_iter(n: u128, vstruct: &VecStruct) -> u128 {
+    let mut k = 999_999_999;
     for i in vstruct.v.iter() {
-        k = *i;
+        k ^= n + i;
     }
     k
 }
 
-pub fn struct_iter_clone(vstruct: &VecStruct) -> u128 {
+pub fn struct_iter_clone(n: u128, vstruct: &VecStruct) -> u128 {
     let mut k = 0;
     for i in vstruct.v.clone() {
-        k = i;
+        k ^= n + i;
     }
     k
 }
 
-pub fn struct_iter_slice(vstruct: &VecStruct) -> u128 {
+pub fn struct_iter_slice(n: u128, vstruct: &VecStruct) -> u128 {
     let mut k = 0;
     for i in &vstruct.v {
-        k = *i;
+        k ^= n + i;
     }
     k
 }
 
-pub fn struct_iter_field(v: &Vec<u128>) -> u128 {
+pub fn struct_iter_field(n: u128, v: &Vec<u128>) -> u128 {
     let mut k = 0;
     for i in v {
-        k = *i;
+        k ^= n + i;
     }
     k
 }
 
-// Result on my machine
-// struct_iter_iter        2.071793839s
-// struct_iter_clone       1.844594912s
-// struct_iter_slice       2.053948301s
-// struct_iter_field       2.064109265s
+// Result on my machine(--release, ITERATIONS=999)
+// struct_iter_iter	1.053240662s, 999999992
+// struct_iter_clone	2.773735007s 999999998
+// struct_iter_slice	1.01796874s 999999992
+// struct_iter_field	1.031684125s 999999998
 //
+// (debug, ITERATIONS=9)
+// T.B.D.
 pub fn test_performance() {
-    println!("Testing performance");
+    const ITERATIONS: u128 = 9;
+    let mut input = String::new();
+    println!("Testing performance, provide integer number:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Please provide integer");
+    let input: u128 = match input.trim().parse() {
+        Ok(n) => n,
+        Err(e) => panic!(e),
+    };
 
     let vstruct = VecStruct {
         v: Vec::from_iter(std::iter::repeat(5).take(999_999)),
     };
 
+    let mut r = 1;
     let start = Instant::now();
-    for i in (0..999_999).enumerate() {
-        struct_iter_iter(&vstruct);
+    for i in (0..ITERATIONS).enumerate() {
+        r ^= struct_iter_iter(input + i.1, &vstruct);
     }
 
     let duration = start.elapsed();
-    println!("struct_iter_iter\t{:?}", duration);
+    println!("struct_iter_iter\t{:?}, {:?}", duration, r);
 
     let start = Instant::now();
-    for i in (0..999_999).enumerate() {
-        struct_iter_clone(&vstruct);
+    for i in (0..ITERATIONS).enumerate() {
+        r ^= struct_iter_clone(input + i.1, &vstruct);
     }
 
     let duration = start.elapsed();
-    println!("struct_iter_clone\t{:?}", duration);
+    println!("struct_iter_clone\t{:?} {:?}", duration, r);
 
     let start = Instant::now();
-    for i in (0..999_999).enumerate() {
-        struct_iter_slice(&vstruct);
+    for i in (0..ITERATIONS).enumerate() {
+        r ^= struct_iter_slice(input + i.1, &vstruct);
     }
 
     let duration = start.elapsed();
-    println!("struct_iter_slice\t{:?}", duration);
+    println!("struct_iter_slice\t{:?} {:?}", duration, r);
 
     let start = Instant::now();
-    for i in (0..999_999).enumerate() {
-        struct_iter_field(&vstruct.v);
+    for i in (0..ITERATIONS).enumerate() {
+        r ^= struct_iter_field(input + i.1, &vstruct.v);
     }
 
     let duration = start.elapsed();
-    println!("struct_iter_field\t{:?}", duration);
+    println!("struct_iter_field\t{:?} {:?}", duration, r);
 }
